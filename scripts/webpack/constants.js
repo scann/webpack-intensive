a5656040ad77a70ade3859795f02ca6471783529
scann <apple.eater01@gmail.com>
2019-03-27 16:32:32 +0200

    Webpack CLI-runnable config with outer constants

---
+++ scripts/webpack/constants.js
@@ -0,0 +1,8 @@
+//Core
+const { resolve } = require('path');
+const { path: PROJECT_ROOT } = require('app-root-path');
+
+//the path(s) that should be cleaned
+exports.PROJECT_ROOT = PROJECT_ROOT;
+exports.SOURCE_DIRECTORY = resolve(PROJECT_ROOT, './source');
+exports.BUILD_DIRECTORY = resolve(PROJECT_ROOT, './build');
--- scripts/webpack/webpack.config.js
+++ scripts/webpack/webpack.config.js
@@ -1,27 +1,32 @@
 //Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const path = require('path');
+
+//Constants
+const { BUILD_DIRECTORY, PROJECT_ROOT, SOURCE_DIRECTORY } = require('./constants');
+
+const cleanOptions = {
+    verbose: true,
+    root:    PROJECT_ROOT,
+};
 
 module.exports = () => {
 
     return {
+        entry:  SOURCE_DIRECTORY,
+        output: {
+            path:     BUILD_DIRECTORY,
+            filename: 'bundle.js',
+        },
         mode:    'none',
         devtool: false,
-        output:  {
-            /**
-             * With zero configuration,
-             *   clean-webpack-plugin will remove files inside the directory below
-             */
-            path: path.resolve(__dirname, 'dist'),
-        },
         plugins: [
-            new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: './static/template.html',
                 title:    'Learning Webpack!🚀',
                 favicon:  './static/favicon.ico',
-            })
+            }),
+            new CleanWebpackPlugin({ BUILD_DIRECTORY, cleanOptions })
         ],
     };
 };
