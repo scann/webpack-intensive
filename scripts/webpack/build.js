0a14d18e6844ba666a24a62d717db0aa4f7fb8af
scann <apple.eater01@gmail.com>
2019-03-27 18:18:25 +0200

    initial nodejs-style webpack config

---
+++ scripts/webpack/build.js
@@ -0,0 +1,49 @@
+// 1.webpack
+// 2.config
+// 3.create webpack compiler
+// 4.run
+
+//Core
+const webpack = require('webpack');
+const chalk = require('chalk');
+
+//Config
+const getConfig = require('./webpack.config');
+
+const compiler = webpack(getConfig());
+
+compiler.run((error, stats) => {
+    if (error) {
+        //configuration error
+        console.error(error.stack || error);
+
+        if (error.details) {
+            console.error(error.details);
+        }
+
+        return null;
+    }
+
+    const info = stats.toString({
+        hash:        true,
+        colors:      true,
+        version:     true,
+        env:         true,
+        modules:     false,
+        entrypoints: false,
+    });
+
+    console.log(chalk.green('Build completed'));
+    console.log(info);
+
+    if (stats.hasErrors()) { //errors during compilation process(broken import, syntax error etc)
+        console.log(chalk.red('--Error!'));
+        console.error(info);
+    }
+
+    if (stats.hasWarnings()) { //warnings during compilation process
+        console.log(chalk.yellow('--Warning!'));
+        console.warn(info);
+    }
+
+});
