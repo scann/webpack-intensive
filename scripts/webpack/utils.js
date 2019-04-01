b1f8da8fbd6723ae736d7d857fdd52151736c249
scann <apple.eater01@gmail.com>
2019-04-01 16:58:02 +0300

    choosePort custom implementation

---
+++ scripts/webpack/utils.js
@@ -0,0 +1,36 @@
+const chalk = require('chalk');
+const detectPort = require('detect-port-alt');
+const inquirer = require('inquirer');
+
+exports.choosePort = async (defaultPort) => {
+    try {
+        const port = await detectPort(defaultPort);
+
+        if (port === defaultPort) {
+            return defaultPort;
+        }
+
+        const message = `Port ${defaultPort} is already in use`;
+
+        if (process.stdout.isTTY) {
+            const questionName = 'changePort';
+            const question = {
+                type:    'confirm',
+                name:    questionName,
+                message: chalk.yellow(`${message}. Do you want to run the app on another port?`),
+                default: true,
+            };
+
+            const result = await inquirer.prompt(question);
+
+            return result[questionName] ? port : null;
+        }
+        console.log(chalk.red(`--${message}`));
+
+    } catch (err) {
+        console.log(chalk.red('--Error!'));
+        console.log(err.message || err);
+    }
+
+    return null;
+};
