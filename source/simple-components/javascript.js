1ac8afc2848740b7b133e1991fbe65f68a8543fd
scann <apple.eater01@gmail.com>
2019-04-02 15:57:38 +0300

    Test js-file added

---
+++ source/simple-components/javascript.js
@@ -0,0 +1,30 @@
+class Human {
+    kind = 'homo sapiens';
+
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayName () {
+        console.log('Name', this.name);
+    }
+
+    sayAge () {
+        console.log('Age', this.age);
+    }
+
+    sayKind = () => {
+        console.log('Kind', this.kind);
+    }
+}
+
+const gendalf = new Human('Gendalf', 666);
+
+gendalf.sayName();
+gendalf.sayAge();
+
+export const a = 1;
+export const b = 5;
+const c = a + b;
+
+console.log(c);
