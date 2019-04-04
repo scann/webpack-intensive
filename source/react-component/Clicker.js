35a3b8782c613feb8b94dd3ce39df56bdf58a211
scann <apple.eater01@gmail.com>
2019-04-04 18:03:26 +0300

    Svg pics added to Clicker component

--- source/react-component/Clicker.js
+++ source/react-component/Clicker.js
@@ -5,6 +5,8 @@ import { hot } from 'react-hot-loader/root';
 // Styles
 import Styles from './postcss.css';
 import kitty from '../theme/images/kitty.jpg';
+import { ReactComponent as ReactLogoComponent } from '../theme/images/react.svg';
+import reactLogo from '../theme/images/react.svg';
 
 import { Button } from './Button';
 
@@ -31,6 +33,9 @@ class Clicker extends Component {
                         '--mainColor':       'rebeccapurple',
                         '--headingFontSize': `${this.state.count}px`,
                     } }>
+                    <ReactLogoComponent width = { 100 } />
+
+                    <img src = { reactLogo } />
                     <img src = { kitty } />
                     <h1>Counter: {count}</h1>
                     <Button onClick = { this.inc }>Increment</Button>
