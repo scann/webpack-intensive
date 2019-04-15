c3e2afca3524e10e0d91c83bd25e2b1bcb1f442d
scann <apple.eater01@gmail.com>
2019-04-15 16:49:33 +0300

    Lazy loading external components example

--- source/simple-components/dom.js
+++ source/simple-components/dom.js
@@ -1,5 +1,18 @@
+import React from 'react';
+
 export default (text = 'Привет! 🎉🎉🎉') => {
     const element = document.createElement('div');
+
+    element.addEventListener('click', async () => {
+
+        element.innerHTML = 'Загрузка...';
+
+        const result = await import(/* webpackChunkName: "lazyLoadedText" */'./lazyLoadedText');
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        element.innerHTML = result.default;
+    });
 
     element.innerHTML = text;
 
